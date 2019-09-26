c943f39681dc0cfd0e3fad394362fcf0841e4308
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-09-26 18:18:46 -0400

    updated

--- drafts/assets/javascript/main.js
+++ drafts/assets/javascript/main.js
@@ -2,20 +2,20 @@ function domReady() {
     document.body.className += ' javascript';
     // code goes here
 
-    if (annyang) {
-        // Let's define our first command. First the text we expect, and then the function it should call
-        var commands = {
-          "hello": function() {
-            console.log("character chosen");
-          }
-        };
-      
-        // Add our commands to annyang
-        annyang.addCommands(commands);
-      
-        // Start listening. You can call this here, or attach this call to an event, button, etc.
-        annyang.start();
-      }
+
+    // Let's define our first command. First the text we expect, and then the function it should call
+    var commands = {
+        "hello": function () {
+            console.log("My name is Elder Price.");
+        }
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+
 
 
 
